220a6c3b1dc434eaaeedbd6e5117629e843af932
m-aung <myothuwinaung1991@gmail.com>
2022-01-22 21:47:15 -0500

    resolved: merge conflicts from Footer.js are resolved

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,37 +2,21 @@ const Footer = () => (
   `
   <footer class="relative text-center text-blue-500 bg-red-300 p-3"> 
     <span class="pt-2 pl-3 pb-2 pr-2 mr-2 bg-blue-300 text-rose-700 hover:text-blue-900"> 
-<<<<<<< HEAD
       <a href="https://www.facebook.com/">
-=======
-      <a href="">
->>>>>>> d47750225407bfb490da396ddc48e934ea00f14f
         <i class="fab fa-facebook-f"></i>
       </a> 
     </span> 
     <span class="pt-2 pl-3 pb-2 pr-2 mr-2 bg-blue-300 text-rose-700 hover:text-blue-900"> 
-<<<<<<< HEAD
       <a href="https://www.instagram.com/">
-=======
-      <a href="">
->>>>>>> d47750225407bfb490da396ddc48e934ea00f14f
         <i class="fab fa-instagram"></i>
       </a> 
       </span> 
       <span class="pt-2 pl-3 pb-2 pr-2 mr-2 bg-blue-300 text-rose-700 hover:text-blue-900">
-<<<<<<< HEAD
           <a href="https://twitter.com/"><i class="fab fa-twitter"></i>
         </a> 
       </span> 
       <span class="pt-2 pl-3 pb-2 pr-3 bg-blue-300 text-rose-700 hover:text-blue-900">
         <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i>
-=======
-          <a href=""><i class="fab fa-twitter"></i>
-        </a> 
-      </span> 
-      <span class="pt-2 pl-3 pb-2 pr-3 bg-blue-300 text-rose-700 hover:text-blue-900">
-        <a href=""><i class="fab fa-youtube"></i>
->>>>>>> d47750225407bfb490da396ddc48e934ea00f14f
         </a> 
       </span> 
   </footer> 
