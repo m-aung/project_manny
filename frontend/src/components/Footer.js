844a23f7a37ffa53e2e31dbd6726e729538c01fd
Myo Aung <myothuwinaung1991@gmail.com>
2022-01-22 21:51:34 -0500

    Updates: social media links are added to Footer.js. (#3)
    * update: social media links are added to Footer.js and merge conflicts on Footer.js and gitignore are resolved

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,21 +2,21 @@ const Footer = () => (
   `
   <footer class="relative text-center text-blue-500 bg-red-300 p-3"> 
     <span class="pt-2 pl-3 pb-2 pr-2 mr-2 bg-blue-300 text-rose-700 hover:text-blue-900"> 
-      <a href="">
+      <a href="https://www.facebook.com/">
         <i class="fab fa-facebook-f"></i>
       </a> 
     </span> 
     <span class="pt-2 pl-3 pb-2 pr-2 mr-2 bg-blue-300 text-rose-700 hover:text-blue-900"> 
-      <a href="">
+      <a href="https://www.instagram.com/">
         <i class="fab fa-instagram"></i>
       </a> 
       </span> 
       <span class="pt-2 pl-3 pb-2 pr-2 mr-2 bg-blue-300 text-rose-700 hover:text-blue-900">
-          <a href=""><i class="fab fa-twitter"></i>
+          <a href="https://twitter.com/"><i class="fab fa-twitter"></i>
         </a> 
       </span> 
       <span class="pt-2 pl-3 pb-2 pr-3 bg-blue-300 text-rose-700 hover:text-blue-900">
-        <a href=""><i class="fab fa-youtube"></i>
+        <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i>
         </a> 
       </span> 
   </footer> 
