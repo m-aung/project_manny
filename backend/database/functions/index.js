d02c2b3ed98677c554e7c6128b5764038bccf7a8
m-aung <myothuwinaung1991@gmail.com>
2022-02-17 09:06:16 -0500

    simple backend setup

--- backend/database/functions/index.js
+++ backend/database/functions/index.js
@@ -64,15 +64,13 @@ const makeTable = (tableName, columnList, tableConstraints) => {
 };
 
 const exampleQuery = `
-  CREATE TABLE accounts (
-    user_id serial PRIMARY KEY,
-    username VARCHAR ( 50 ) UNIQUE NOT NULL,
-    password VARCHAR ( 50 ) NOT NULL,
-    email VARCHAR ( 255 ) UNIQUE NOT NULL,
+  CREATE TABLE videoes (
+    video_id serial PRIMARY KEY,
+    title VARCHAR ( 50 ) UNIQUE NOT NULL,
+    video_link VARCHAR ( 250 ) NOT NULL,
+    views INT NOT NULL,
     created_on TIMESTAMP NOT NULL,
-    last_login TIMESTAMP 
-    FOREIGN KEY (user_id)
-    REFERENCES accounts (user_id)
+    uploaded_on TIMESTAMP NOT NULL
 );`;
 
 const newTable = makeTable(
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,38 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.SERVER_PORT || 8080;
+const PORT = process.env.SERVER_PORT || 5000;
 
-app.get('/', (req, res) =>
-  res.send(JSON.stringify({ message: 'Hello World' }))
-);
+const localData = require('./database/database.json');
+console.log(JSON.stringify(localData));
+app.get('/', (req, res) => {
+  res.send(JSON.stringify({ message: 'Hello World' }));
+});
 
 app.get('/api/', (req, res) => {
   const { firstname, lastname, email } = req.query;
-  res
-    .send(JSON.stringify({ firstname, lastname, email }))
-    .redirect('http://localhost:3030/');
+  res.send(JSON.stringify({ firstname, lastname, email }));
+  // .redirect('http://localhost:3030/');
 });
-app.get('/videoList/latest/:datefrom', (req, res) => {
+app.get('/videos/latest/', (req, res) => {
   console.log(req.param);
-
   // query data from database
-  res.send(JSON.stringify(data));
+  res.send(JSON.stringify(localData));
 });
-
+app.get('/videos/features/', (req, res) => {
+  // query data from database
+  res.send(JSON.stringify(localData));
+});
+app.get('/videos/spotlight/', (req, res) => {
+  // query data from database
+  res.send(JSON.stringify(localData));
+});
+app.get('/videos/all/', (req, res) => {
+  // query data from database
+  res.send(JSON.stringify(localData));
+});
+app.post('/videos/edit/post-video', (req, res) => {
+  console.log('Request Body:', req.body);
+  res.send(JSON.stringify({ message: 'Posted!' }));
+});
+//:title-:video_link-:created-on
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
